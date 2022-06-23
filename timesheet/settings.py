8b81b9815d59dcc990b187f7e6c8795676e69690
Isha Patil <68316775+ishathepatil@users.noreply.github.com>
2022-06-23 03:10:11 -0400

    Update settings.py

--- timesheet/settings.py
+++ timesheet/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-+thy7vl$5m1i)*_q6_&)sj(s((em&=_u(^)m8g0diq=9zhz7@l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
